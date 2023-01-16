1f79b3688028a966f09a29a172be11fd6e564536
Pranav Lad <87913792+pranav-lad@users.noreply.github.com>
2023-01-16 15:26:16 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -9,7 +9,7 @@ function setgradient(){
      + color1.value+
      ","+
       color2.value+")"
-      css.textContent = body.style.background + ";";
+      css.textContent = body.style.background + ";"; 
 }
 color1.addEventListener("input",setgradient)
 color2.addEventListener("input",setgradient)
